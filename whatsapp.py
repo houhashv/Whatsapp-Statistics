dc574d1b97d8eb0d54cb30bd42623ff69f79daf0
houhashv <yossihohashvili1@gmail.com>
2018-03-21 22:48:18 +0200

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,77 +1,54 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 16 08:23:56 2017
+Created on Wed Mar 21 20:40:53 2018
 
-@author: yossi
+@author: user
 """
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
 from numpy import math
+import re
 
-with open("C:\\Users\\yossi\\Desktop\\_chat111.txt",encoding="utf-8") as file:
+with open("C:\\Users\\user\\Desktop\\_chat.txt",encoding="utf-8") as file:
     
     data = []
-    num = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+    num = [str(x) for x in range(1,13)]
     
     for line in file:
+        lines_c += 1
+        search = re.search("(\d{1,2}).(\d{1,2}).(\d{4}),"+\
+                           " (\d{1,2}):(\d{2}):(\d{2})",line)
+
+        print(search if search==None else search.groups())
         temp = line   
         line = line[:-1].split(':')
-        #breaking down the date
-        first = line[0].split(', ')
-        line.remove(line[0])
-        hour = [first[-1]]
-        #breaking down the time
-        if isinstance(line,list)==True and len(line)>=1:
-            minute = [line[0]]
-            line.remove(line[0])
-        if isinstance(line,list)==True and len(line)>=1:
-            sec = [line[0]]
-            line.remove(line[0])
-        date = first[0].split('.')
-        line = date+hour+minute+sec+line
-        if len(line)>=9:
-            line[-2] = line[-2]+":"+line[-1]
-            line.remove(line[-1])
-            if len(line)==9:
-                line[-2] = line[-2]+":"+line[-1]
-                line.remove(line[-1])     
-        #chars counting
-        words = []
-        if line[-1]!=" <‏התמונה הושמטה>" and line[-1]!=" <‏קטע קול הושמט>" and line[-1]!=" <‏הסרט הושמט>":
-            words.append(len(line[-1].strip(" ")))
+        if len(line)>=4:
+            sender = line[2][4:]
+            message = ":".join(line[3:])
+            sums += len(line)
+            row = [x for x in search.groups()]+[sender,message,\
+                   len(message)]
+            if "<‏התמונה הושמטה>" in message:
+                row+=[1,0,0]
+            else:
+                if " <‏קטע קול הושמט>" in message:
+                    row+=[0,1,0]
+                else:
+                    if " <‏הסרט הושמט>" in message:
+                        row+=[0,0,1] 
+                    else:
+                        row+=[0,0,0]
+            data.append(row)
         else:
-            words.append(0)
-        
-        line = line+words
-        #movies, pictures and voacl messages counting 
-        isVoice = []
-        isPic = []
-        isMovie = []
+            if len(line)==1:
+                count+=1
+                sums +=len(line)
+                print(line[0])
 
-        if line[-2]==" <‏התמונה הושמטה>":
-            isPic.append(1)
-        else:
-            isPic.append(0)
-        if line[-2]==" <‏קטע קול הושמט>":
-            isVoice.append(1)
-        else:
-            isVoice.append(0)
-        if line[-2]==" <‏הסרט הושמט>":
-            isMovie.append(1)
-        else:
-            isMovie.append(0)
-                
-        line = line+isPic+isVoice+isMovie
-        #new line use case handeling
-        if len(data)> 0 and (len(line)<len(data[-1]) or len(line)>12 or line[1] not in num):
-            data[-1][-5] = data[-1][-5]+" "+temp
-            data[-1][-4] = len(data[-1][-5].strip(" "))
-        else:
-            data.append(line)
-#creatung the data frame            
-pd = pd.DataFrame(data,columns=["day","month","year","hour","minute","second","sender","message","chars","isPic","isVoice","isMovie"])
-#
-result = pd.groupby(["sender"])["chars"].mean().sort_values(axis=0,ascending=False)
+df = pd.DataFrame(data,columns=["day","month",\
+                                "year","hour","minute","second",\
+                                "sender","message","chars",\
+                                "pic","voice","video"])
 
      
